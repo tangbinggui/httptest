c9e7d6274d35c000ce35b10b75704c9411d65902
tangbinggui <919841076@qq.com>
2019-06-15 19:07:00 +0800

    修改内容

--- test_git/src/com/itheima/git/Git.java
+++ test_git/src/com/itheima/git/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("我真猛");
         System.out.println("我真强");
+
+        System.out.println("我真几把强");
     }
 }
